a1fb280addcfba9335e7bfde01bfd19b59e6d18a
Smr2dhi <smr2dhiya@mail>
2026-08-20 18:53:43 +0530

    hard1

--- Assign5/hard_1.py
+++ Assign5/hard_1.py
@@ -11,9 +11,79 @@ policies = [
 ] 
 
 @app.get("/")
-def policy():
+def home():
     return{
     "services":"Policy API",
     "version":"1.0",
     "endpoints":[]
 }
+
+@app.get("/policies")
+
+
+def get_policies(status:str=""):
+    result=[]
+
+    for query in policies:
+        if query["active"] and status=="active" :
+            result.append(query)
+
+        elif status=="inactive" and not query["active"]:
+            result.append(query)
+
+        elif status=="":
+            result.append(query)
+    
+    return{
+            "count":len(result),
+            "results":result
+                }
+       
+@app.get("/policies/search")
+def search_policy(customer:str="",type:str="",max_premium:float=0):
+    result=[]
+    for user in policies:
+        if user["customer"].lower()==customer.lower()  and user["type"].lower()==type.lower()and user["premium"]<=max_premium:
+            result.append(user)
+
+    return {
+        "count":len(result),
+        "result":result}
+
+
+@app.get("/policies/stats")
+def policy_stats():
+    total_policies=len(policies)
+    active_count=0
+    inactive_count=0
+    active_preium_total=0
+
+    for policy in policies:
+        if policy["active"]==True:
+            active_preium_total+=policy["premium"]
+            active_count+=1
+        elif  policy["active"]==False:
+            inactive_count+=1
+
+    return{
+            "total":total_policies,
+            "active":active_count,
+            "inactive":inactive_count,
+            "active_premium_total":active_preium_total
+        }
+
+@app.get("/policies/{policy_number}")
+def get_policy(policy_number):
+    for policy in policies:
+        if policy["policy_number"]==policy_number:
+            return policy
+
+    return {"error:Policy not found"}
+
+@app.get("/policies/customer/{name}")
+def get_name(name=str):
+    result=[]
+    for user in policies:
+        if user["customer"].lower()==name.lower():
+            result.append(user)
+    return result
